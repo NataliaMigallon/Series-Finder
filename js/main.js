5641920c8d069630a0b7a9c2cf440f8f8489ea3d
NataliaMigallon <natmigallon@gmail.com>
2021-02-08 17:34:58 +0100

    Add first commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const moviesContainer = document.querySelector(".js-movies-container");
+const inputElement = document.querySelector(".js-input");
+const formElement = document.querySelector(".js-form");
